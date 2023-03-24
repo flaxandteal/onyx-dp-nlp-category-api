6ec47fa3859a823ed35c10fa5cbd8febc46c2d88
Phil Weir <phil.weir@flaxandteal.co.uk>
2023-03-24 08:19:05 +0000

    tidy up split

--- core/ff_fasttext/settings.py
+++
@@ -1,8 +0,0 @@
-from dynaconf import LazySettings
-
-settings = LazySettings(
-    DEBUG_LEVEL_FOR_DYNACONF="DEBUG",
-    ENVVAR_PREFIX_FOR_DYNACONF="FF_FASTTEXT_CORE",
-    DOTENV_PATH_FOR_DYNACONF=".env",
-)
-settings.reload()
