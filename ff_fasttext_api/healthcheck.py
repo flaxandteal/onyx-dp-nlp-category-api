597102a5fca9a805bed5b80350c90fb251825cfa
kamen.dimitrov1 <kamen.flaxandteal.co.uk>
2023-03-30 10:39:24 +0300

    updated python healthcheck

--- ff_fasttext_api/healthcheck.py
+++ ff_fasttext_api/healthcheck.py
@@ -1,4 +1,7 @@
 from datetime import datetime
+import subprocess
+import time
+import sys
 
 # Define the check statuses
 OK = 'OK'
@@ -8,25 +11,51 @@ ERROR = 'ERROR'
 class Healthcheck:
     def __init__(self, status, version, uptime, start_time, checks):
         self.status = status
-        self.version = version
+        self.version = {
+            "version": version,
+            "git_commit": self.get_last_commit(),
+            "language": "python",
+            "language_version": sys.version,
+        }
         self.uptime = uptime
         self.start_time = start_time
         self.checks = checks
 
     def to_json(self):
-        now = datetime.utcnow().isoformat()
+        now = datetime.utcnow()
 
-        uptime_sec = self.uptime
-
-        # Define the healthcheck response
         response = {
             'status': self.status,
             'version': self.version,
-            'uptime': uptime_sec,
+            'uptime': self.get_uptime(self.uptime),
             'start_time': self.start_time,
-            'checks': self.checks,
-            'timestamp': now
+            'timestamp': now,
+            'checks': self.checks
         }
 
-        # Convert the response to JSON and return it
         return response
+    
+    def get_last_commit(self):
+        last_commit = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('utf-8').strip()
+        return last_commit
+    
+    def get_uptime(self, start_time):
+        uptime = time.time()
+        intervals = (
+            ('w', 604800),
+            ('d', 86400),
+            ('h', 3600),
+            ('m', 60),
+            ('s', 1),
+        )
+        uptime = round(uptime - start_time)
+
+        parts = []
+        for name, count in intervals:
+            value = int(uptime // count)
+            if value:
+                uptime -= value * count
+                parts.append(f"{value} {name}")
+
+        return ', '.join(parts)
+    
