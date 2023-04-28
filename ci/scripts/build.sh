2b108e13a91bce834881044502cf253da0ce0cfe
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-04-28 10:21:55 +0300

    updated build to remove cached go updated script to only build bin

--- ci/scripts/build.sh
+++ ci/scripts/build.sh
@@ -1,11 +1,5 @@
 #!/bin/bash -eux
 
-cwd=$(pwd)
-
-export GOPATH=$cwd/go
-
 pushd dp-nlp-category-api
   make build-bin
-  mkdir $cwd/build
-  cp dist/ff_fasttext_api-0.0.1-py3-none-any.whl ff_fasttext_api-0.0.1.tar.gz $cwd/build
 popd
