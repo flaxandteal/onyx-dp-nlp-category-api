6670ee3f73fdbca68960700cd72c7740fda1e623
kamen.dimitrov1 <kamen.dimitrov@flaxandteal.co.uk>
2023-06-13 14:30:55 +0300

    made changes to settings

--- tests/unit/settings_test.py
+++ tests/unit/settings_test.py
@@ -2,5 +2,4 @@ from ff_fasttext_api.settings import settings
 
 def test_load_settings():
     assert settings.load_dotenv is True
-    assert settings.DEBUG_LEVEL_FOR_DYNACONF == "DEBUG"
     assert settings.ENVVAR_PREFIX_FOR_DYNACONF == 'FF_FASTTEXT_API'
